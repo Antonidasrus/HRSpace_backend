5e7fc474f22a4d82061be06b523df18a00e410d9
unknown <yasha1bz@gmail.com>
2024-03-26 12:28:28 +0300

    clearing_

--- hrspace_backend/app/validators.py
+++ hrspace_backend/app/validators.py
@@ -1,9 +1,9 @@
-from django.core.exceptions import ValidationError
-from django.utils import timezone
+# from django.core.exceptions import ValidationError
+# from django.utils import timezone
 
 
-def date_validator(date):
-    if date <= timezone.datetime.now().date() + timezone.timedelta(3):
-        raise ValidationError(
-            "Дата должна быть больше текущей даты на три дня"
-        )
+# def date_validator(date):
+#     if date <= timezone.datetime.now().date() + timezone.timedelta(3):
+#         raise ValidationError(
+#             "Дата должна быть больше текущей даты на три дня"
+#         )
